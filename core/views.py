92047e1d85a7d4cdd6947282261aa90f9d52e4ef
Luciano <oluciano.net@gmail.com>
2020-11-28 17:36:55 -0300

    Criando a atualização (UPDATE)

---
+++ core/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+from core.models import Task
+from core.forms import FormTask
+
+
+def read(request):
+    objects = Task.objects.all()
+    context = {'objects': objects}
+    return render(request, 'read.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = FormTask(request.POST)
+        if form.is_valid():
+            task = form.save()
+            return redirect('details', id=task.id)
+    form = FormTask()
+    return render(request, 'form.html', context={'form': form})
+
+
+def details(request, id):
+    object = get_object_or_404(Task, id=id)
+    return render(request, 'details.html', context={'object': object})
+
+
+def delete(request, id):
+    object = get_object_or_404(Task, id=id)
+    object.delete()
+    return redirect('read')
+
+
+def update(request, id):
+    task = get_object_or_404(Task, id=id)
+
+    if request.method == 'POST':
+        form = FormTask(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return redirect('details', id=task.id)
+    form = FormTask(instance=task)
+    return render(request, 'form.html', context={'form': form})
+
+
+    object.delete()
+    return redirect('read')
